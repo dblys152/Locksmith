8ab413450957bc58440d10cb6b7798d5bf7e8283
youngseok <dblys152@gmail.com>
2025-07-12 23:54:19 +0900

    ..

---
+++ src/main/java/com/ys/locksmith/LocksmithApplication.java
@@ -0,0 +1,13 @@
+package com.ys.locksmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocksmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocksmithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ys/locksmith/LocksmithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.locksmith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocksmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
